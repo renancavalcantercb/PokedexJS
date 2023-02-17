995a9bda7bab76cd45716b5ad6154321f03b3571
renancavalcantercb <renancavalcantercb@protonmail.com>
2023-02-17 16:25:53 -0300

    better poison color

--- src/App.js
+++ src/App.js
@@ -27,24 +27,24 @@ const displayError = (error) => (
 )
 
 const typeColors = {
-	normal: '#A8A77A',
-	fire: '#EE8130',
-	water: '#6390F0',
-	electric: '#F7D02C',
-	grass: '#7AC74C',
-	ice: '#96D9D6',
-	fighting: '#C22E28',
-	poison: '#A33EA1',
-	ground: '#E2BF65',
-	flying: '#A98FF3',
-	psychic: '#F95587',
-	bug: '#A6B91A',
-	rock: '#B6A136',
-	ghost: '#735797',
-	dragon: '#6F35FC',
-	dark: '#705746',
-	steel: '#B7B7CE',
-	fairy: '#D685AD',
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#D7A1F9",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD"
 };
 
 function getTypeColor(type) {
@@ -110,6 +110,7 @@ const displayPokemonList = (pokemonList, handleSearch) => (
 
 const pokemonDetails = (selectedPokemon, handleClear, toggleFavorite, starIcon) => {
   const textColor = getTextColor(selectedPokemon);
+  console.log(getTypeColor(selectedPokemon.types[0].type.name))
   return (
     <Card
       key={selectedPokemon.name}
